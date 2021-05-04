703fb8a31377204f7807cdaf1ca91eba6b824125
unknown <valery.lapeza@gmail.com>
2021-05-04 19:07:33 +0300

    add files

---
+++ Homework_1/my_script.bash
@@ -0,0 +1,8 @@
+#! /bin/bash
+cd /c/Users/adl1k/git #перейти в папку
+mkdir folder_git1 folder_git2 folder_git3 #создать 3 папки
+cd QApractice #зайти в папку
+touch Legolas.txt Gimli.txt Aragorn.txt Frodo.json Samwise.json #создать 5 файлов (3 txt, 2 json)
+mkdir folder_git4 folder_git5 folder_git6 #создать 3 папки
+ls -la #вывести содержимое папки
+mv Legolas.txt Aragorn.txt folder_git6 #переместить любые 2 файла, которые создали, в любую другую папку
